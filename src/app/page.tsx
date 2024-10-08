f928da4c2e16a1e8b75ec57a76c73b1369f7c3bc
taco-tortilla <tortillataco0@gmail.com>
2024-10-08 20:36:26 +0900

    update: change wording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Top() {
     <div className='flex h-screen items-center justify-center'>
       <div className='text-center'>
         <h1 className='pb-5 text-2xl font-bold tablet:text-4xl'>
-          Hey, How are u?
+          How are u?
         </h1>
         <h2 className='text-lg font-medium tablet:text-xl'>
           I live with nature :)
