a6aba4b0b5a09ae8dbd5e3ee8aa628ecefd27b0e
taco-tortilla <tortillataco0@gmail.com>
2024-10-19 18:47:05 +0900

    fix: home title position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 export default function Top() {
   return (
-    <div className='flex h-screen items-center justify-center'>
+    <div className='absolute left-1/2 top-1/2 flex h-screen -translate-x-1/2 -translate-y-1/2 items-center justify-center'>
       <div className='text-center'>
-        <h1 className='pb-5 text-2xl font-bold tablet:text-4xl'>
-          How are u?
-        </h1>
+        <h1 className='pb-5 text-2xl font-bold tablet:text-4xl'>How are u?</h1>
         <h2 className='text-lg font-medium tablet:text-xl'>
           I live with nature :)
         </h2>
